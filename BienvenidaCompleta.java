759df874fe25e9bae85e8379447a18d5f9d51ece
CaaInd635 <132603384+CaaInd635@users.noreply.github.com>
2023-05-05 11:19:18 -0500

    Update BienvenidaCompleta.java

--- BienvenidaCompleta.java
+++ BienvenidaCompleta.java
@@ -3,9 +3,9 @@ public class HolaMundo
 	public static void main(String args[]) {
 		
 		System.out.println("""
-			 				  				   ¡Hola mundo! 
-								Bienvenido al curso de Programacion Web 2
-											de Carlos Quenta 
+			 		¡Hola mundo! 
+			 Bienvenido al curso de Programacion Web 2
+				      de Carlos Quenta 
 		""");
   	}
 }
